af48b1a0989f473d05cf60468cc600a4d338da0d
Yura <ybeliaev@gmail.com>
2019-06-28 13:54:36 +0300

    add someData

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class App extends React.Component {
+  minID = 10;
+  state = {
+    someData: [
+      { label: "first label", important: false, done: false, id: this.minID++ },
+      {
+        label: "second label",
+        important: false,
+        done: false,
+        id: this.minID++
+      },
+      { label: "third label", important: false, done: false, id: this.minID++ },
+      { label: "for label", important: false, done: false, id: this.minID++ }
+    ]
+  };
+}
